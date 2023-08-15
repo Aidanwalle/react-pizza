09f7e451c4f894b1911acaa539e73052a0e083c4
Aidanwalle <dionis.serdiukov@icloud.com>
2023-08-15 18:05:46 +0300

    add lodash.debounce

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -9,10 +9,17 @@ import style from "./Search.module.scss";
 
 const Search = () => {
   const { searchValue, setSearchValue } = React.useContext(SearchContext);
+  const inputRef = React.useRef();
+
+  const onClickClear = () => {
+    setSearchValue("");
+    inputRef.current.focus();
+  };
 
   return (
     <div className={style.searchBlock}>
       <input
+        ref={inputRef}
         value={searchValue}
         onChange={(event) => setSearchValue(event.target.value)}
         type="text"
@@ -21,7 +28,7 @@ const Search = () => {
       <img className={style.search} src={searchLogo} alt="search" />
       {searchValue && (
         <img
-          onClick={() => setSearchValue("")}
+          onClick={onClickClear}
           className={style.clear}
           src={clearSearch}
           alt="clear"
